7ef00625416883425659fc80076336321208986e
joon <sargadil@gmail.com>
2023-11-18 19:46:31 +0900

    Add overflow-hidden to layout container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex w-screen h-screen justify-center items-center overflow-hidden">
-        <div className="relative flex flex-col w-screen min-w-[300px] max-w-[600px] h-screen min-h-[500px] max-h-[900px] justify-items-center items-center border-sky-500 border-2 rounded-2xl">
+        <div className="relative flex flex-col w-screen min-w-[300px] max-w-[600px] h-screen min-h-[500px] max-h-[900px] justify-items-center items-center border-sky-500 border-2 rounded-2xl overflow-hidden">
           <Header />
           {children}
           <Footer />
